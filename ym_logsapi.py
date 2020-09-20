3bb7775b94c9ed0d879f86964982701093587e5a
vlbo <vl.boyarintsev@gmail.com>
2020-09-20 10:36:14 +0300

    Update ym_logsapi.py

--- ym_logsapi.py
+++ ym_logsapi.py
@@ -11,10 +11,8 @@ headers = {
     #'Content-Length': '123'
     }
 
-path = '/Users/vl.boyarintsev/Documents/GitHub/hello_world/'
-
 start_of_url = 'https://api-metrika.yandex.ru/management/v1/counter/55237969/'
-date1 = '2020-01-01'
+date1 = '2018-01-01'
 date2 = '2020-09-11'
 fields = "ym:s:clientID,ym:s:dateTime,ym:s:lastTrafficSource,ym:s:visitDuration,ym:s:pageViews"
 source = 'visits'
@@ -22,11 +20,11 @@ oauth_token = 'AgAAAAAoVX5fAAaY0CYtJ-9qfki_tcfmAXyYLhA'
 
 # запрос на формирование
 url_create = start_of_url+'logrequests?'+'date1='+date1+'&date2='+date2+'&fields='+fields+'&source='+source+'&oauth_token='+oauth_token
-'''
+
 result = requests.post(url_create, headers=headers) # запрос к API
 result = result.text.encode('utf-8').decode('utf-8') # расшифровка ответа
 result_to_dict = json.loads(result) # превращаем ответ в словарь python, чтобы потом вытащить requestID
-'''
+
 # возвращаем текущие обращения к серверу
 url_get = start_of_url+'logrequests?'
 result = requests.get(url_get, headers=headers)
@@ -35,18 +33,16 @@ result = result.text.encode('utf-8').decode('utf-8') # расшифровка о
 result = json.loads(result) # превращаем ответ в словарь python, чтобы потом вытащить кол-во частей
 request_id = result['requests'][0]['request_id']
 
-'''
-# удаляем сформированные отчет с сервера
-delete_url = start_of_url+'/logrequest/'+str(request_id)+'/clean?oauth_token='+oauth_token
-delete = requests.post(delete_url,headers=headers)
-json.loads(delete.text.encode('utf-8').decode('utf-8'))
-'''
-
 if result['requests'][0]['status'] == 'processed':
     download_url = start_of_url+'/logrequest/'+str(request_id)+'/part/0'+'/download?&oauth_token='+oauth_token
     download_result = requests.get(download_url, headers=headers)                      # запрос к API
     download_result = download_result.text.encode('utf-8').decode('utf-8')  # расшифровка ответа
 else: print('Отчет еще не сформирован')
+
+# удаляем сформированные отчет с сервера
+delete_url = start_of_url+'/logrequest/'+str(request_id)+'/clean?oauth_token='+oauth_token
+delete = requests.post(delete_url,headers=headers)
+json.loads(delete.text.encode('utf-8').decode('utf-8'))
 
 from io import StringIO
 df = pd.read_csv(StringIO(download_result), sep='\t', header=0)
